b5afd5955d61c09666934b9d075d8aca42fb2e93
luanhsr <luanhenriquework@gmail.com>
2024-07-22 19:54:16 -0300

    corpo HTML Simple Calculator

---
+++ src/components/simpleCalculator.jsx
@@ -0,0 +1,29 @@
+import style from './SimpleCalculator.module.css'
+export function MianCalculate () {
+
+    return (
+        <div className={style.boxMain}>
+            <div>
+                <header className={style.header}>
+                    <h1>Simple Calculator</h1>
+                    <p>Calculate your simple interest Easily</p>
+                </header>
+                <main>
+                <div className={style.displayCalc}>
+                    <strong>₹ 10.000</strong>
+                    <p>Simple Interest</p>
+                </div>
+                <div>
+                    <form action="#" method="post">
+                        <input type="text" name="Principal" id="Principal" placeholder="Principal amount" />
+                        <input type="text" name="Rate" id="Rate" placeholder="Rate of interest (p.a)"/>
+                        <input type="number" name="Time Period" id="Time" placeholder="3"/>
+                    </form>
+                    
+                </div>
+                </main>
+            </div>
+        </div>
+    )
+
+}
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import { MianCalculate } from './components/simpleCalculator'
+import ReactDOM from 'react-dom/client'
+import './global.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <div>
+      <MianCalculate>
+
+      </MianCalculate>
+  </div>
+
+)
